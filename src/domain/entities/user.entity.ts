6147bee824a271988ab18574c7b4cf1f526bc0f0
4cnbologi4 <daniell9404@outlook.com>
2025-05-27 11:49:51 -0600

    Add vercel config v2

--- src/domain/entities/user.entity.ts
+++
@@ -1,6 +0,0 @@
-export class UserEntity {
-  id: string;
-  name: string;
-  email: string;
-  createdAt: Date;
-} 
--- src/domain/interfaces/user.repository.interface.ts
+++
@@ -1,8 +0,0 @@
-import { UserEntity } from '../entities/user.entity';
-import { CreateUserDto } from '../dtos/create-user.dto';
- 
-export interface UserRepositoryInterface {
-  create(user: CreateUserDto): Promise<UserEntity>;
-  findAll(): Promise<UserEntity[]>;
-  findById(id: string): Promise<UserEntity | null>;
-} 
